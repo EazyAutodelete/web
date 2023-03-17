c179bc6791db1934454b0a047b49ade20ac2859c
Ben Schiemann <dev@qreepex.xyz>
2023-03-17 14:19:59 +0100

    fix(stats): use timeout in stats requests

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,20 +6,6 @@ $("#supportButton").on("click", function () {
   window.location.href = "https://eazyautodelete.xyz/discord/";
 });
 
-const statOptions = {
-  day: "today",
-  week: "last 7 days",
-  month: "last 30 days",
-  year: "last year",
-};
-
-const statsCache = {
-  day: 0,
-  week: 0,
-  month: 0,
-  year: 0,
-};
-
 const animationDuration = 1750;
 
 const animateDeletedMessages = (start, end, duration) => {
@@ -43,50 +29,6 @@ const animateDeletedMessages = (start, end, duration) => {
   timer = setInterval(run, stepTime);
   run();
 };
-
-$("#stats").on("click", function () {
-  const statsElem = $(this);
-  const currentStat = statsElem.attr("data-stat");
-  const nextStat =
-    Object.keys(statOptions)[(Object.keys(statOptions).indexOf(currentStat) + 1) % Object.keys(statOptions).length];
-
-  statsElem.attr("data-stat", nextStat);
-  statsElem.html(statOptions[nextStat]);
-
-  if (statsCache[nextStat] != 0) {
-    animateDeletedMessages(0, statsCache[nextStat], animationDuration / 3);
-  } else {
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        stats = Math.round(JSON.parse(this.responseText).count / 1e3) * 1e3;
-        animateDeletedMessages(0, stats, animationDuration / 3);
-
-        statsCache[nextStat] = stats;
-      }
-    };
-    request.open("GET", "https://api.eazyautodelete.xyz/public/stats/deleted-messages/" + nextStat, false);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send();
-  }
-});
-
-try {
-  let initStatsRequest = new XMLHttpRequest();
-  initStatsRequest.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      stats = Math.round(JSON.parse(this.responseText).count / 1e3) * 1e3;
-      animateDeletedMessages(0, stats, animationDuration);
-
-      statsCache["week"] = stats;
-    }
-  };
-  initStatsRequest.open("GET", "https://api.eazyautodelete.xyz/public/stats/deleted-messages/week", false);
-  initStatsRequest.setRequestHeader("Content-Type", "application/json");
-  initStatsRequest.send();
-} catch (e) {
-  console.log(e);
-}
 
 $(document).ready(function () {
   ((text, write_duration) => {
@@ -120,7 +62,7 @@ $(document).ready(function () {
     }
     timer = setInterval(run, stepTime);
     run();
-  })(0, 1750, animationDuration);
+  })(0, 1950, animationDuration);
 
   $("#mode-1").prop("src", "https://cdn.eazyautodelete.xyz/assets/02568459-37c5-436a-a82b-b6e4c92c6c59");
   $("#mode-2").prop("src", "https://cdn.eazyautodelete.xyz/assets/02568459-37c5-436a-a82b-b6e4c92c6c59");
