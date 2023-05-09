9c8ea7d8e3085125db21b85caa5c8e2e2392fa2d
Qreepex <dev@qreepex.xyz>
2023-05-09 17:41:58 +0200

    feat(guilds): fetch guild count from api

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,27 +42,33 @@ $(document).ready(function () {
     }, write_duration / text.length);
   })("Discord Bot to automatically delete messages.", animationDuration);
 
-  ((start, end, duration) => {
-    let obj = document.getElementById("server-count");
-    let range = end - start;
-    let minTimer = 50;
-    let stepTime = Math.abs(Math.floor(duration / range));
-    stepTime = Math.max(stepTime, minTimer);
-    let startTime = new Date().getTime();
-    let endTime = startTime + duration;
-    let timer;
+  (() => {
+    let guilds = 2e3;
+    const request = new XMLHttpRequest();
+    request.timeout = 500;
+    request.onreadystatechange = function () {
+      4 == this.readyState &&
+        200 == this.status &&
+        (guilds = 100 * Math.round(JSON.parse(this.responseText).count / 100));
+    };
+    request.open("GET", "https://api.eazyautodelete.xyz/public/stats/guilds", !0);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send();
+    const object = document.getElementById("server-count");
+    const s = Math.max(Math.abs(Math.floor(animationDuration / guilds)), 50);
+
+    let interval;
+    const l = new Date().getTime() + animationDuration;
     function run() {
-      let now = new Date().getTime();
-      let remaining = Math.max((endTime - now) / duration, 0);
-      let value = Math.round(end - remaining * range);
-      obj.innerHTML = value.toLocaleString() + "+";
-      if (value == end) {
-        clearInterval(timer);
-      }
+      const a = new Date().getTime();
+      const s = Math.max((l - a) / animationDuration, 0);
+      const r = Math.round(guilds - s * guilds);
+      object.innerHTML = r.toLocaleString() + "+";
+      r == guilds && clearInterval(interval);
     }
-    timer = setInterval(run, stepTime);
+    interval = setInterval(run, s);
     run();
-  })(0, 1950, animationDuration);
+  })();
 
   $("#mode-1").prop("src", "https://cdn.eazyautodelete.xyz/assets/02568459-37c5-436a-a82b-b6e4c92c6c59");
   $("#mode-2").prop("src", "https://cdn.eazyautodelete.xyz/assets/02568459-37c5-436a-a82b-b6e4c92c6c59");
