8c331b5c29ef94e34ec1a32f2d7120587ecc7da0
Ben Schiemann <dev@qreepex.xyz>
2024-10-24 20:55:31 +0200

    feat(redirects): add more unlocalized redirects

---
+++ src/routes/use-cases-examples/+server.ts
@@ -0,0 +1,11 @@
+import { locales } from "$lib/const.js";
+import { getPage } from "$lib/i18n.js";
+import { redirect } from "@sveltejs/kit";
+
+/** @type {import('./$types').RequestHandler} */
+export function GET({ request }) {
+	let locale = request.headers.get("accept-language")?.split(",")[0]?.split("-")[0];
+	if (!locale || !locales.includes(locale)) locale = "en";
+
+	throw redirect(301, `/${locale}/${getPage("use-cases-examples", locale)}`);
+}
